b4a502445b94aaca970899e207991de130b2165e
mtastan <magjantastanov@gmail.com>
2025-09-20 02:47:17 +0500

    delete, update user

--- architecture/repository/user/deleteByID.go
+++ architecture/repository/user/deleteByID.go
@@ -1,5 +1,25 @@
 package user
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 func (u *UserRepo) DeleteByID(id int64) error {
+
+	result, err := u.DB.Exec(`DELETE FROM users WHERE id = ?`, id)
+	if err != nil {
+		return fmt.Errorf("DeleteByID: %w", err)
+	}
+
+	// проверим, был ли реально удалён пользователь
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("DeleteByID: %w", err)
+	}
+	if rowsAffected == 0 {
+		return sql.ErrNoRows // никого не удалили → такого id нет
+	}
+
 	return nil
 }
--- architecture/repository/user/update.go
+++ architecture/repository/user/update.go
@@ -1,9 +1,31 @@
 package user
 
-import "real-time-forum/architecture/models"
+import (
+	"database/sql"
+	"fmt"
+	"real-time-forum/architecture/models"
+)
 
 func (u *UserRepo) Update(user *models.User) error {
+	// формируем SQL-запрос
+	result, err := u.DB.Exec(`
+		UPDATE users 
+		SET nickname = ?, email = ?, password = ?, updated_at = ? 
+		WHERE id = ?`,
+		user.Nickname, user.Email, user.Password, user.UpdatedAt.Format(models.TimeFormat), user.ID,
+	)
+	if err != nil {
+		return fmt.Errorf("Update: %w", err)
+	}
+
+	// проверяем, реально ли обновилась строка
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("Update: %w", err)
+	}
+	if rowsAffected == 0 {
+		return sql.ErrNoRows // если пользователь с таким ID не найден
+	}
+
 	return nil
 }
-
-/////////////////
